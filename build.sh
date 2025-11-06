408c2e2b046e58e01f7c3741b10dc1ec26f75b9e
Eugene Maslovich <ehpc@ehpc.io>
2025-11-06 14:56:30 +0300

    Add support for custom format in resume build

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 lualatex -jobname=resume-en "\def\resumeLang{en}\input{resume.tex}"
 lualatex -jobname=resume-ru "\def\resumeLang{ru}\input{resume.tex}"
 lualatex -jobname=resume-fr "\def\resumeLang{fr}\input{resume.tex}"
+lualatex -jobname=resume-ru-nt "\def\resumeLang{ru}\def\customSpacing{wide}\def\noTutor{true}\input{resume.tex}"
